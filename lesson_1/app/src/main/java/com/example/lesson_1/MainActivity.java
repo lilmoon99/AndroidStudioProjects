ed62950a1f21f6cf2fb011bfc2c6ae8793e3a170
lilmoon99 <83182720+lilmoon99@users.noreply.github.com>
2023-02-06 18:25:52 +0400

    lesson 1 hw

---
+++ lesson_1/app/src/main/java/com/example/lesson_1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lesson_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        code for edit_text_layout
+        setContentView(R.layout.edit_text_layout);
+
+//        code for activity_main
+//        setContentView(R.layout.activity_main);
+
+    }
+}
