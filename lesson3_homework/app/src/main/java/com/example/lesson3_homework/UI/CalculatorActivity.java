58a673761c2d91592be798cea657dd01d631b68f
lilmoon99 <83182720+lilmoon99@users.noreply.github.com>
2023-02-16 19:19:02 +0400

    lesson 3 homework

---
+++ lesson3_homework/app/src/main/java/com/example/lesson3_homework/UI/CalculatorActivity.java
@@ -0,0 +1,93 @@
+package com.example.lesson3_homework.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.lesson3_homework.R;
+import com.example.lesson3_homework.model.CalculatorImpl;
+import com.example.lesson3_homework.model.Operator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculatorActivity extends AppCompatActivity implements CalculatorView {
+
+    private TextView resultTxt;
+
+    private CalculatorPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        resultTxt = findViewById(R.id.result);
+
+        presenter = new CalculatorPresenter(this, new CalculatorImpl());
+
+        Map<Integer, Integer> digits = new HashMap<>();
+        digits.put(R.id.key_1, 1);
+        digits.put(R.id.key_2, 2);
+        digits.put(R.id.key_3, 3);
+        digits.put(R.id.key_4, 4);
+        digits.put(R.id.key_5, 5);
+        digits.put(R.id.key_6, 6);
+        digits.put(R.id.key_7, 7);
+        digits.put(R.id.key_8, 8);
+        digits.put(R.id.key_9, 9);
+        digits.put(R.id.key_0, 0);
+        View.OnClickListener digitClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.onDigitPressed(digits.get(view.getId()));
+            }
+        };
+
+        findViewById(R.id.key_1).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_2).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_3).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_4).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_5).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_6).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_7).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_8).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_9).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_0).setOnClickListener(digitClickListener);
+
+        Map<Integer, Operator> operators = new HashMap<>();
+        operators.put(R.id.key_add, Operator.ADD);
+        operators.put(R.id.key_sub, Operator.SUB);
+        operators.put(R.id.key_mul, Operator.MULT);
+        operators.put(R.id.key_div, Operator.DIV);
+        operators.put(R.id.key_equal_to,Operator.EQUAL_TO);
+
+        View.OnClickListener operatorClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.onOperatorPressed(operators.get(view.getId()));
+                presenter.onEqualPressed(operators.get(view.getId()));
+            }
+        };
+
+        findViewById(R.id.key_add).setOnClickListener(operatorClickListener);
+        findViewById(R.id.key_sub).setOnClickListener(operatorClickListener);
+        findViewById(R.id.key_mul).setOnClickListener(operatorClickListener);
+        findViewById(R.id.key_div).setOnClickListener(operatorClickListener);
+        findViewById(R.id.key_equal_to).setOnClickListener(operatorClickListener);
+
+        findViewById(R.id.key_dot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.onDotPressed();
+            }
+        });
+    }
+
+    @Override
+    public void showResult(String result) {
+        resultTxt.setText(result);
+    }
+}
---
+++ lesson3_homework/app/src/main/java/com/example/lesson3_homework/UI/CalculatorView.java
@@ -0,0 +1,5 @@
+package com.example.lesson3_homework.UI;
+
+public interface CalculatorView {
+    void showResult(String result);
+}
