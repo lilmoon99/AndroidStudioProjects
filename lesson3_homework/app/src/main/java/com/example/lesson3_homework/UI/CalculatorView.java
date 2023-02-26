bf95b7781b9535bbf2d60dac9dbbd75fa51dbf10
lilmoon99 <83182720+lilmoon99@users.noreply.github.com>
2023-02-26 12:23:57 +0400

    lesson 3 init commit

--- lesson3_homework/app/src/main/java/com/example/lesson3_homework/UI/CalculatorView.java
+++
@@ -1,5 +0,0 @@
-package com.example.lesson3_homework.UI;
-
-public interface CalculatorView {
-    void showResult(String result);
-}
--- lesson3_homework/app/src/main/java/com/example/lesson3_homework/model/Calculator.java
+++
@@ -1,6 +0,0 @@
-package com.example.lesson3_homework.model;
-
-public interface Calculator {
-
-    double perform(double arg1, double arg2, Operator operator);
-}
