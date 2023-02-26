fbf67848ff641d0707515943bd2e7e0e7f7eba83
lilmoon99 <83182720+lilmoon99@users.noreply.github.com>
2023-02-26 12:59:32 +0400

    lesson 3 init

---
+++ lesson_3_hw/app/src/main/java/com/example/lesson_3_hw/model/Calculator.java
@@ -0,0 +1,6 @@
+package com.example.lesson_3_hw.model;
+
+public interface Calculator {
+
+    double perform(double arg1, double arg2, Operator operator);
+}
---
+++ lesson_3_hw/app/src/main/java/com/example/lesson_3_hw/model/Operator.java
@@ -0,0 +1,8 @@
+package com.example.lesson_3_hw.model;
+public enum Operator {
+    ADD,
+    SUB,
+    MULT,
+    DIV,
+    EQUAL_TO
+}
